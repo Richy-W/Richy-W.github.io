dcbeca7b5c27275cfd4c21abc5aada1b2e804a9a
Richy W <75549147+Richy-W@users.noreply.github.com>
2023-05-05 15:20:42 -0500

    Add files via upload

--- RPS.js
+++ RPS.js
@@ -1,14 +1,20 @@
-const compChoiceDisplay = document.getElementById('comp-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultsDisplay = document.getElementById('result')
-const possibleChoice = document.querySelectorAll('button')
-let userChoice
-let compChoice
-let result
+const compChoiceDisplay = document.getElementById('comp-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultsDisplay = document.getElementById('result');
+const possibleChoice = document.querySelectorAll('button');
+const CountW = document.getElementById('wins');
+const CountL = document.getElementById('lose');
+const GetDraw = document.getElementById('draw');
+let userChoice;
+let compChoice;
+let result;
+var winCount = 0;
+var loseCount = 0;
+var drawCount = 0;
 
 possibleChoice.forEach(possibleChoice => possibleChoice.addEventListener('click',(e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
     generateCompChoice()
     getResult()
 }))
@@ -17,42 +23,62 @@ function generateCompChoice() {
     const randoNumb = Math.floor(Math.random() * 3) + 1
 
     if (randoNumb === 1) {
-        compChoice = 'Rock'
+        compChoice = 'Rock';
     }
     
     if (randoNumb === 2) {
-        compChoice = 'Paper'
+        compChoice = 'Paper';
     }
     
     if (randoNumb === 3) {
-        compChoice = 'Scissors'
+        compChoice = 'Scissors';
     }
 
-    compChoiceDisplay.innerHTML = compChoice
+    compChoiceDisplay.innerHTML = compChoice;
 
 }
 
 function getResult() {
-    if(compChoice === userChoice)
-    result = 'Draw!'
 
-    if(compChoice === 'Rock' && userChoice === 'Scissors')
-    result = 'You Lose!'
 
-    if(compChoice === 'Scissors' && userChoice === 'Paper')
-    result = 'You Lose!'
+    if(compChoice === userChoice) {
+    result = 'Draw!';
+    drawCount ++;
+    };
 
-    if(compChoice === 'Paper' && userChoice === 'Rock')
-    result = 'You Lose!'
+    if(compChoice === 'Rock' && userChoice === 'Scissors') {
+    result = 'You Lose!';
+    loseCount ++;
+    };
 
-    if(compChoice === 'Rock' && userChoice === 'Paper')
-    result = 'You Win!'
+    if(compChoice === 'Scissors' && userChoice === 'Paper') {
+    result = 'You Lose!';
+    loseCount ++;
+    };
 
-    if(compChoice === 'Scissors' && userChoice === 'Rock')
-    result = 'You Win!'
+    if(compChoice === 'Paper' && userChoice === 'Rock') {
+    result = 'You Lose!';
+    loseCount ++;
+    };
 
-    if(compChoice === 'Paper' && userChoice === 'Scissors')
-    result = 'You Win!'
+    if(compChoice === 'Rock' && userChoice === 'Paper') {
+    result = 'You Win!';
+    winCount ++;
+    }
 
-    resultsDisplay.innerHTML = result
+    if(compChoice === 'Scissors' && userChoice === 'Rock') {
+    result = 'You Win!';
+    winCount ++;
+    };
+
+    if(compChoice === 'Paper' && userChoice === 'Scissors') {
+    result = 'You Win!';
+    winCount ++;
+    };
+
+    resultsDisplay.innerHTML = result;
+    CountW.innerHTML = winCount;
+    CountL.innerHTML = loseCount;
+    GetDraw.innerHTML = drawCount;
+    
 }
